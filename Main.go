b31a24c9d2ec41b285b1b5f227a51c07492320ab
Álvaro Rodrigo <36777501+AlKiam@users.noreply.github.com>
2020-04-01 23:44:26 +0200

    Update Main.go

--- Main.go
+++ Main.go
@@ -10,7 +10,7 @@ func main() {
 
 	fmt.Println(os.Args[1:])
 	for i, val := range os.Args[1:] {
-	data, err := ioutil.ReadFile(val)
+		data, err := ioutil.ReadFile(val)
 
 		if err != nil {
 			fmt.Println("[Error]: Cannot read the file:", err, i)
