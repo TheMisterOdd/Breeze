d63e5340bfbd9d0da06782582a00c8e98f977835
Álvaro Rodrigo <36777501+AlKiam@users.noreply.github.com>
2020-04-01 23:42:22 +0200

    Add files via upload

--- Main.go
+++ Main.go
@@ -2,11 +2,22 @@ package main
 
 import (
 	"fmt"
+	"io/ioutil"
 	"os"
 )
 
 func main() {
 
-	fmt.Println(os.Args[1:]);
+	fmt.Println(os.Args[1:])
+	for i, val := range os.Args[1:] {
+		data, err := ioutil.ReadFile(val)
+
+		if err != nil {
+			fmt.Println("[Error]: Cannot read the file:", err, i)
+			return
+		}
+
+		fmt.Println(string(data))
+	}
 
 }
