e6d24e8ea2bb44cb8c7baa0dbe5a0c76c9866450
Álvaro Rodrigo <36777501+AlKiam@users.noreply.github.com>
2020-04-02 12:30:05 +0200

    Update leivac.c

--- leivac.c
+++ leivac.c
@@ -3,28 +3,28 @@
 
 char* bytes_load(const char* _Src)
 {
-    char* _Buffer = NULL;
-    FILE* fp = fopen(_Src, "rb");
+	char* _Buffer = NULL;
+    	FILE* fp = fopen(_Src, "rb");
 
-    if (!fp) 
-        return NULL;
+    	if (!fp) 
+        	return NULL;
     
-    fseek(fp, 0, SEEK_END);
-    long length = ftell(fp);
-    fseek(fp, 0, SEEK_SET);
+    	fseek(fp, 0, SEEK_END);
+    	long length = ftell(fp);
+    	fseek(fp, 0, SEEK_SET);
 
-    _Buffer = (char*)malloc(sizeof(char) * length);
-    fread(_Buffer, length, sizeof(char), fp);
-    fclose(fp);
+    	_Buffer = (char*)malloc(sizeof(char) * length);
+    	fread(_Buffer, length, sizeof(char), fp);
+    	fclose(fp);
 
-    if (!_Buffer) 
-    {   
-        puts("[Error]: Cannot read data, and data was NULL");
-        free(_Buffer);
-        return NULL;
-    }
+    	if (!_Buffer) 
+    	{   
+        	puts("[Error]: Cannot read data, and data was NULL");
+        	free(_Buffer);
+        	return NULL;
+    	}
 
-    return _Buffer;
+    	return _Buffer;
 }
 
 int main(int args, char** argv) 
